2dce0ceb0e2e439f72fb965c95e4542ff59e7cf2
igor <foolu6972@gmail.com>
2026-07-26 00:02:37 +0300

    url string added, and style issue

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,7 +6,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 ENV_FILE_PATH = Path(__file__).resolve().parent.parent.parent / ".env"
 
 
-
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=ENV_FILE_PATH, env_file_encoding="utf-8")
 
--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker 
+from app.core.config import settings
 
 
-engine = create_async_engine(..., echo=True)
+engine = create_async_engine(settings.database_url, echo=True)
 
 SessionLocal = async_sessionmaker(bind=engine)
