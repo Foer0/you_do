fa7ff230736e844746d89e5b3813fa68f0efbeb9
igor <foolu6972@gmail.com>
2026-07-26 18:40:57 +0300

    add new fields

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
+
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
 
 ENV_FILE_PATH = Path(__file__).resolve().parent.parent.parent / ".env"
 
@@ -9,6 +9,11 @@ ENV_FILE_PATH = Path(__file__).resolve().parent.parent.parent / ".env"
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=ENV_FILE_PATH, env_file_encoding="utf-8")
 
+    db_user: str = Field(default=...)
+    db_passwd: str = Field(default=...)
+    db_host: str = Field(default=...)
+    db_port: int = Field(default=...)
+    db_name: str = Field(default=...)
     database_url: str = Field(default=...)
 
 
