ec6f301b5a2a5f33edb917bbd2b6a2a37123407c
igor <foolu6972@gmail.com>
2026-08-05 12:45:33 +0300

    typo in parameter corrected

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta, timezone
 
 import jwt
+from argon2 import PasswordHasher
 
 from app.core.config import settings
 
@@ -14,5 +15,16 @@ def create_access_token(user_id: int) -> str:
 
 
 def decode_access_token(token: str) -> int:
-    payload = jwt.decode(token, settings.secret_key, algorithm=[ALGORITHM])
+    payload = jwt.decode(token, settings.secret_key, algorithms=[ALGORITHM])
     return int(payload["sub"])
+
+
+def hash_passwd(passwd: str) -> str:
+    ph = PasswordHasher()
+    hashed_passwd = ph.hash(passwd)
+    return hashed_passwd
+
+
+def verify_passwd(passwd: str, hash_passwd: str) -> None:
+    ph = PasswordHasher()
+    ph.verify(hash_passwd, passwd)
