c6c4e480a768ef591d89883510926376319be8b1
igor <foolu6972@gmail.com>
2026-08-10 16:12:09 +0300

    Added a parameter to avoid data expiration

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,7 +1,7 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker 
-from app.core.config import settings
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
+from app.core.config import settings
 
 engine = create_async_engine(settings.database_url, echo=True)
 
-SessionLocal = async_sessionmaker(bind=engine)
+SessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
